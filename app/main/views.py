65888bcdd89dcc07f7372d76b7e9a6454cff9394
jasonmuchiri <jasonmkinyua@gmail.com>
2019-05-03 09:52:38 +0300

    Make changes in the app

--- app/main/views.py
+++ app/main/views.py
@@ -2,10 +2,3 @@ from flask import render_template, request, redirect, url_for, abort, flash
 from . import main
 from ..models import User
 from flask_login import login_required
-
-
-
-@main.route('/')
-
-def index():
-    return render_template('index.html')
--- app/models.py
+++ app/models.py
@@ -32,3 +32,4 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+        
